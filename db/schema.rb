08aba3c745612519b75f0f430f52cc8b80b1f841
Viki Harrod <victoria.harrod@gmail.com>
2015-01-12 18:44:41 -0700

    implement Guard and legislator factory, add legislator model

---
+++ db/schema.rb
@@ -0,0 +1,35 @@
+# encoding: UTF-8
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# Note that this schema.rb definition is the authoritative source for your
+# database schema. If you need to create the application database on another
+# system, you should be using db:schema:load, not running all the migrations
+# from scratch. The latter is a flawed and unsustainable approach (the more migrations
+# you'll amass, the slower it'll run and the greater likelihood for issues).
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 20150112215121) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "legislators", force: :cascade do |t|
+    t.string   "bioguide_id"
+    t.boolean  "in_office"
+    t.string   "party"
+    t.string   "first_name"
+    t.string   "last_name"
+    t.string   "middle_name"
+    t.string   "nickname"
+    t.string   "gender"
+    t.string   "state"
+    t.string   "title"
+    t.string   "chamber"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
+
+end
