dc02925238c3504f40cb494e616ed6ea69fc0bd2
Viki Harrod <victoria.harrod@gmail.com>
2015-01-14 16:34:56 -0700

    add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,13 +7,43 @@ class Seed
   end
 
   def generate_legislators
-    FactoryGirl.create(:legislator, first_name: "Diana",
-    last_name: "DeGette",
-    middle_name: "L.",
-    gender: "F",
-    state: "CO",
-    title: "Rep",
-    chamber: "house")
+    @degette = FactoryGirl.create(:legislator, first_name: "Diana",
+      last_name: "DeGette",
+      middle_name: "L.",
+      gender: "F",
+      state: "CO",
+      title: "Rep",
+      chamber: "house")
+
+    @gardner = FactoryGirl.create(:legislator, party: "R",
+      first_name: "Cory",
+      last_name: "Gardner",
+      gender: "M",
+      state: "CO",
+      title: "Sen",
+      chamber: "senate")
+
+    @bennet = FactoryGirl.create(:legislator, party: "D",
+      first_name: "Michael",
+      last_name: "Bennet",
+      middle_name: "F.",
+      gender: "M",
+      state: "CO",
+      title: "Sen",
+      chamber: "senate")
+
+    puts "legislators generated"
   end
 
+  def generate_contributors
+    3.times do
+      FactoryGirl.create(:contributor, legislator: @degette)
+      FactoryGirl.create(:contributor, legislator: @gardner)
+      FactoryGirl.create(:contributor, legislator: @bennet)
+    end
+
+    puts "contributors generated"
+  end
 end
+
+Seed.new
