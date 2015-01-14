6b1986b2c713def12c329517146f69d953b7f85d
Viki Harrod <victoria.harrod@gmail.com>
2015-01-14 12:41:21 -0700

    fix inconsistent single/double quotes

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,8 @@ Rails.application.routes.draw do
 
   get '/auth/:provider/callback', to: 'sessions#create'
 
-  get '/login' => redirect("/auth/facebook"), as: :login
+  get '/login' => redirect('/auth/facebook'), as: :login
+  get '/logout' => 'sessions#destroy', as: :logout
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
