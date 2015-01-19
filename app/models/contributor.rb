31d1300e7fbc763d6e6a3de5f4f7b468f87a8059
Viki Harrod <victoria.harrod@gmail.com>
2015-01-19 14:43:37 -0700

    pull out some partials, add election_cycles helper

--- app/models/contributor.rb
+++ app/models/contributor.rb
@@ -3,8 +3,4 @@ class Contributor < ActiveRecord::Base
   validates :legislator, presence: true
 
   scope :cycle, -> (year) { where(cycle: year) }
-
-  def self.cycles
-    ["1990", "1992", "1994", "1996", "1998", "2000", "2002", "2004", "2006", "2008", "2010", "2012", "2014"]
-  end
 end
