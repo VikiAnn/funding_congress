737cd174624ed0a4fbc250265045bf9d06591344
Viki Harrod <victoria.harrod@gmail.com>
2015-01-19 15:12:34 -0700

    1. Make method in user model not a class method 2. Change method name to update_from_omniauth and make it only update things 3. Call that method instead of the old class method 5. Fix broken specs

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,14 +1,9 @@
 class User < ActiveRecord::Base
-  def self.find_or_create_by_auth(auth_data)
-    user = find_or_create_by(uid: auth_data["uid"])
-
-    user.email      = auth_data["info"]["email"]
-    user.name       = auth_data["info"]["name"]
-    user.image_url  = auth_data["info"]["image"]
-    user.token      = auth_data["credentials"]["token"]
-    user.provider   = auth_data["provider"]
-    user.save
-
-    user
+  def update_from_omniauth(auth_data)
+    self.email      = auth_data["info"]["email"]
+    self.name       = auth_data["info"]["name"]
+    self.image_url  = auth_data["info"]["image"]
+    self.token      = auth_data["credentials"]["token"]
+    self.provider   = auth_data["provider"]
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe User, :type => :model do
-  it "can be found or created from an oauth hash" do
+  it "can be updated from an oauth hash" do
     auth_data = { "provider" => "facebook",
       "uid" => "0123456789101112",
       "info" =>
@@ -12,10 +12,14 @@ RSpec.describe User, :type => :model do
           "image" => "http://graph.facebook.com/0123456789101112/picture",
       "credentials" =>
         { "token"=> "apiufbgPIUABERPIUBGiaubgpiaerubpiUBAPRIUBGiubpfiegbuPISUBEriubsa" } }
-    user = User.find_or_create_by_auth(auth_data)
+
+    user = create(:user, uid: auth_data["uid"])
+    user.update_from_omniauth(auth_data)
+    user.save!
+    user.reload
+
     expect(user.name).to eq auth_data["info"]["name"]
     expect(user.email).to eq auth_data["info"]["email"]
-    expect(user.uid).to eq auth_data["uid"]
     expect(user.token).to eq auth_data["credentials"]["token"]
   end
 end
