e3646822bda8aedefb9c10057926229c137d6f0c
Viki Harrod <victoria.harrod@gmail.com>
2015-01-22 09:40:06 -0700

    change _data to _hash in legislators/contributors ingesters, change json variable to reflect plurality

--- app/services/contributors_ingester.rb
+++ app/services/contributors_ingester.rb
@@ -1,34 +1,34 @@
 class ContributorsIngester
-  attr_reader :raw_contributor_json, :legislator_id
+  attr_reader :raw_contributors_json, :legislator_id
 
-  def initialize(raw_contributor_json, legislator_id)
-    @raw_contributor_json = raw_contributor_json
+  def initialize(raw_contributors_json, legislator_id)
+    @raw_contributors_json = raw_contributors_json
     @legislator_id = legislator_id
   end
 
   def ingest
-    raw_contributor_json.map do |contributor_data|
-      find_or_create_contributors(legislator_id, contributor_data).tap do |contributor|
-        update_and_save_contributors(contributor, contributor_data)
+    raw_contributors_json.map do |contributor_hash|
+      find_or_create_contributors(legislator_id, contributor_hash).tap do |contributor|
+        update_and_save_contributors(contributor, contributor_hash)
       end
     end
   end
 
-  def self.ingest(raw_contributor_json, legislator_id)
-    new(raw_contributor_json, legislator_id).ingest
+  def self.ingest(raw_contributors_json, legislator_id)
+    new(raw_contributors_json, legislator_id).ingest
   end
 
   private
 
-  def find_or_create_contributors(legislator_id, contributor_data) 
-    Contributor.where(legislator_id: legislator_id, sunlight_id: contributor_data[:id], cycle: contributor_data[:cycle]).first_or_create
+  def find_or_create_contributors(legislator_id, contributor_hash) 
+    Contributor.where(legislator_id: legislator_id, sunlight_id: contributor_hash[:id], cycle: contributor_hash[:cycle]).first_or_create
   end
 
-  def update_and_save_contributors(contributor, contributor_data)
-    contributor.name            = contributor_data[:name]
-    contributor.employee_amount = contributor_data[:employee_amount]
-    contributor.total_amount    = contributor_data[:total_amount]
-    contributor.direct_amount   = contributor_data[:direct_amount]
+  def update_and_save_contributors(contributor, contributor_hash)
+    contributor.name            = contributor_hash[:name]
+    contributor.employee_amount = contributor_hash[:employee_amount]
+    contributor.total_amount    = contributor_hash[:total_amount]
+    contributor.direct_amount   = contributor_hash[:direct_amount]
     contributor.save!
   end
 end
--- app/services/legislators_ingester.rb
+++ app/services/legislators_ingester.rb
@@ -1,44 +1,44 @@
 class LegislatorsIngester
-  attr_reader :raw_legislator_json
+  attr_reader :raw_legislators_json
 
-  def initialize(raw_legislator_json)
-    @raw_legislator_json = raw_legislator_json
+  def initialize(raw_legislators_json)
+    @raw_legislators_json = raw_legislators_json
   end
 
   def ingest
-    raw_legislator_json["results"].map do |legislator_data|
-      find_or_create_legislators(legislator_data).tap do |legislator|
-        update_and_save_legislators(legislator, legislator_data)
+    raw_legislators_json["results"].map do |legislator_hash|
+      find_or_create_legislators(legislator_hash).tap do |legislator|
+        update_and_save_legislators(legislator, legislator_hash)
       end
     end
   end
 
-  def self.ingest(raw_legislator_json)
-    new(raw_legislator_json).ingest
+  def self.ingest(raw_legislators_json)
+    new(raw_legislators_json).ingest
   end
 
   private
 
-  def find_or_create_legislators(legislator_data)
-    Legislator.where(bioguide_id: legislator_data["bioguide_id"]).first_or_create
+  def find_or_create_legislators(legislator_hash)
+    Legislator.where(bioguide_id: legislator_hash["bioguide_id"]).first_or_create
   end
 
-  def update_and_save_legislators(legislator, legislator_data)
-    legislator.in_office   = legislator_data["in_office"]
-    legislator.party       = legislator_data["party"]
-    legislator.first_name  = legislator_data["first_name"]
-    legislator.last_name   = legislator_data["last_name"]
-    legislator.middle_name = legislator_data["middle_name"]
-    legislator.nickname    = legislator_data["nickname"]
-    legislator.state       = legislator_data["state"]
-    legislator.title       = legislator_data["title"]
-    legislator.chamber     = legislator_data["chamber"]
-    legislator.facebook_id = legislator_data["facebook_id"]
-    legislator.twitter_id  = legislator_data["twitter_id"]
-    legislator.youtube_id  = legislator_data["youtube_id"]
-    legislator.phone       = legislator_data["phone"]
-    legislator.office      = legislator_data["office"]
-    legislator.website     = legislator_data["website"]
+  def update_and_save_legislators(legislator, legislator_hash)
+    legislator.in_office   = legislator_hash["in_office"]
+    legislator.party       = legislator_hash["party"]
+    legislator.first_name  = legislator_hash["first_name"]
+    legislator.last_name   = legislator_hash["last_name"]
+    legislator.middle_name = legislator_hash["middle_name"]
+    legislator.nickname    = legislator_hash["nickname"]
+    legislator.state       = legislator_hash["state"]
+    legislator.title       = legislator_hash["title"]
+    legislator.chamber     = legislator_hash["chamber"]
+    legislator.facebook_id = legislator_hash["facebook_id"]
+    legislator.twitter_id  = legislator_hash["twitter_id"]
+    legislator.youtube_id  = legislator_hash["youtube_id"]
+    legislator.phone       = legislator_hash["phone"]
+    legislator.office      = legislator_hash["office"]
+    legislator.website     = legislator_hash["website"]
     legislator.save!
   end
 end
