8c09ee14384cfd653de3421c46644c2ab0935a4d
Viki Harrod <victoria.harrod@gmail.com>
2015-01-24 17:26:20 -0700

    hook up zipcode field on home page to actually work, reword front page message

--- app/controllers/legislators_controller.rb
+++ app/controllers/legislators_controller.rb
@@ -1,9 +1,11 @@
 class LegislatorsController < ApplicationController
   def index
-    if current_user && current_user.zipcode
+    if params["zipcode"]
+      @legislators = Legislator.for_zipcode(params["zipcode"])
+    elsif current_user && current_user.zipcode
       @legislators = Legislator.for_zipcode(current_user.zipcode)
     else
-      @legislators = Legislator.for_zipcode(params["zipcode"])
+      @legislators = Legislator.all
     end
     @year = params["cycle"] || "2014"
   end
