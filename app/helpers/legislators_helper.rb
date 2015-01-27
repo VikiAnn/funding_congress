68ceee257decf4878fb042dff930596a577dd634
Viki Harrod <victoria.harrod@gmail.com>
2015-01-27 14:53:09 -0700

    remove old helper method, update spec

--- app/helpers/legislators_helper.rb
+++ app/helpers/legislators_helper.rb
@@ -1,8 +1,4 @@
 module LegislatorsHelper
-  def election_cycles
-    (1990..Date.today.year).select {|year| year.even?}.map(&:to_s)
-  end
-
   def active(cycle, current)
     if cycle == current
       "class=active"
--- spec/helpers/legislators_helper_spec.rb
+++ spec/helpers/legislators_helper_spec.rb
@@ -1,11 +1,9 @@
 require 'rails_helper'
 
 describe LegislatorsHelper do
-  describe "#cycle" do
-    it "generates a current list of campaign cycle years" do
-      Timecop.freeze(Time.local(2015)) do
-        expect(helper.election_cycles).to eq ["1990", "1992", "1994", "1996", "1998", "2000", "2002", "2004", "2006", "2008", "2010", "2012", "2014"] 
-      end
+  describe "#active" do
+    it "changes tab class to active when cycle is equal to current selected year" do
+      expect(active("2014", "2014")).to eq("class=active")
     end
   end
 end
