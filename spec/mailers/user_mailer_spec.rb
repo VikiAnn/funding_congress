e164653961d9b84c5ce0533d9b509dd06b4537cb
Viki Harrod <victoria.harrod@gmail.com>
2015-01-27 15:52:39 -0700

    add welcome email

--- spec/mailers/user_mailer_spec.rb
+++ spec/mailers/user_mailer_spec.rb
@@ -2,11 +2,13 @@ require "rails_helper"
 
 RSpec.describe UserMailer, :type => :mailer do
   it "sends a welcome email" do
-    email = UserMailer.welcome("viki@example.com").deliver_now
+    email_hash = { email: "viki@example.com", user_name: "Viki Ann" }
+    email = UserMailer.welcome(email_hash).deliver_now
 
     expect(ActionMailer::Base.deliveries).not_to be_empty
-    expect(email.from).to eq("no-reply@fundingcongress.com")
-    expect(email.to).to eq("viki@example.com")
+    expect(email.from[0]).to eq("victoria.harrod@gmail.com")
+    expect(email.to[0]).to eq("viki@example.com")
     expect(email.subject).to eq("Welcome to FundingCongress.com")
+    expect(email.body).to include("Viki Ann")
   end
 end
