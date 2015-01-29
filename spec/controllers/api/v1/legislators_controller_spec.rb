8a8f99d27d9391252b1ff77ea1f477deaa5504fa
Viki Harrod <victoria.harrod@gmail.com>
2015-01-28 17:37:28 -0700

    add show request to api

--- spec/controllers/api/v1/legislators_controller_spec.rb
+++ spec/controllers/api/v1/legislators_controller_spec.rb
@@ -29,4 +29,14 @@ RSpec.describe Api::V1::LegislatorsController, :type => :controller do
     expect(JSON.parse(response.body)["legislators"].count).to eq(1)
     expect(JSON.parse(response.body)["legislators"].first["campaign_contributors"].first.count).to eq(2)
   end
+
+  it "returns a single legislator with its contributors" do
+    legislator = create(:legislator)
+    2.times { create(:contributor, legislator: legislator, cycle: "2014") }
+
+    get(:show, { format: :json, id: legislator.id })
+
+    expect(JSON.parse(response.body).count).to eq(1)
+    expect(JSON.parse(response.body)["legislator"]["campaign_contributors"]["2014"].count).to eq(2)
+  end
 end
